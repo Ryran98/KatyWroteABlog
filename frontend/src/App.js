755a9f5568a269d972d3063d90d6c645b2b3f08d
Ryan Wilson <ryan.wilson0198@gmail.com>
2022-06-28 22:55:52 +0100

    cleardown and refresh

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Home } from "./containers/home";
+import { Travel } from "./containers/travel";
+import { Recipies } from "./containers/recipies";
+import { VanLife } from "./containers/vanlife";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/travel">
+            <Travel />
+          </Route>
+          <Route exact path="/recipies">
+            <Recipies />
+          </Route>
+          <Route exact path="/vanlife">
+            <VanLife />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
